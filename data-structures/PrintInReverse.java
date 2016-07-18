4393c046b37aba2f277ab908c4bad711c7a7dbfa
Vishnu Valleru <vishnuvalleru@gmail.com>
2016-07-17 20:26:21 -0700

    committing removed files.

--- data-structures/PrintInReverse.java
+++ data-structures/PrintInReverse.java
@@ -1,8 +1,5 @@
 void ReversePrint(Node head) {
-  // This is a "method-only" submission. 
-  // You only need to complete this method.
-  
-    if (head == null)
+   if (head == null)
         return;
     ReversePrint(head.next);
     System.out.println(head.data);
